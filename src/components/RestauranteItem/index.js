6c4021e9f15ec9824b803850b7277addfb999af7
João Victor <victoralmeida.0216@gmail.com>
2022-03-01 20:49:45 -0300

    Ifood Clone

---
+++ src/components/RestauranteItem/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Text } from 'react-native';
+import { RestauranteView, RestauranteFoto, RestauranteInfo } from './styles';
+import {AntDesign} from '@expo/vector-icons';
+
+
+const RestauranteItem = ({foto, nome, key, nota, categoria, distancia, valorFrete, tempoEntrega}) => {
+  return (
+    <RestauranteView key={key}>
+      <RestauranteFoto source={{
+        uri: foto.trim(),
+        width: 50,
+        height: 50,
+        resizeMode: 'cover'
+      }}
+      />
+      <RestauranteInfo>
+        <Text>{nome}</Text>
+        <Text> <AntDesign name='star' size={12} color='#F9A825'/> {nota} - {categoria} - {distancia}</Text>
+        <Text>{tempoEntrega} . R$ {valorFrete}</Text>
+        <Text></Text>
+      </RestauranteInfo>
+    </RestauranteView>
+  );
+}
+
+export default RestauranteItem;
